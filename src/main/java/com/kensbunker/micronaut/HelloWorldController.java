ec20439cd2efa5c31150283e768f9f26cda96e87
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-11-09 19:40:01 +0800

    Initial commit

---
+++ src/main/java/com/kensbunker/micronaut/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.kensbunker.micronaut;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/hello")
+public class HelloWorldController {
+
+  @Get("/")
+  public String index() {
+    return "Hello World";
+  }
+
+}
---
+++ src/test/java/com/kensbunker/micronaut/HelloWorldControllerTest.java
@@ -0,0 +1,35 @@
+package com.kensbunker.micronaut;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class HelloWorldControllerTest {
+
+    @Inject
+    EmbeddedApplication application;
+
+    @Inject
+    @Client("/")
+    RxHttpClient client;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+    @Test
+    void testHelloResponse() {
+        final String result = client.toBlocking().retrieve("/hello");
+        assertEquals("Hello World", result);
+    }
+
+}
