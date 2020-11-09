b055cd5c453015c67b96b8f28ead5caa62f03f98
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-11-09 20:55:47 +0800

    Logging

--- src/main/java/com/kensbunker/micronaut/Application.java
+++ src/main/java/com/kensbunker/micronaut/Application.java
@@ -1,10 +1,15 @@
 package com.kensbunker.micronaut;
 
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
+        LOG.info("Hello");
     }
 }
--- src/main/java/com/kensbunker/micronaut/HelloWorldService.java
+++ src/main/java/com/kensbunker/micronaut/HelloWorldService.java
@@ -2,13 +2,24 @@ package com.kensbunker.micronaut;
 
 import io.micronaut.context.annotation.Property;
 import io.micronaut.context.annotation.Value;
+import io.micronaut.context.event.StartupEvent;
+import io.micronaut.runtime.event.annotation.EventListener;
 import javax.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
 public class HelloWorldService {
 
+  private static final Logger LOG = LoggerFactory.getLogger(HelloWorldService.class);
+
   @Property(name = "hello.service.greeting", defaultValue = "default value")
   private String greeting;
+
+  @EventListener
+  public void onStartup(StartupEvent startupEvent) {
+    LOG.debug("Startup: {}", HelloWorldService.class.getSimpleName());
+  }
 
   public String sayHi() {
     return greeting;
