4e7c2797f46166713a885600a6aa0517dc405f08
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-11-09 22:12:17 +0800

    Integrate Lombok

--- src/main/java/com/kensbunker/micronaut/GreetingConfig.java
+++ src/main/java/com/kensbunker/micronaut/GreetingConfig.java
@@ -3,24 +3,19 @@ package com.kensbunker.micronaut;
 import io.micronaut.context.annotation.ConfigurationInject;
 import io.micronaut.context.annotation.ConfigurationProperties;
 import javax.validation.constraints.NotBlank;
+import lombok.Getter;
 
 @ConfigurationProperties("hello.config.greeting")
 public class GreetingConfig {
 
+  @Getter
   private final String de;
+  @Getter
   private final String en;
 
   @ConfigurationInject
   public GreetingConfig(@NotBlank final String de, @NotBlank final String en) {
     this.de = de;
     this.en = en;
-  }
-
-  public String getDe() {
-    return de;
-  }
-
-  public String getEn() {
-    return en;
   }
 }
