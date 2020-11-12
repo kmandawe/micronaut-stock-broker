b675667397e3392466922ce3494d75ff1b80517e
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-11-12 11:50:35 +0800

    Custom HttpResponse

--- src/main/java/com/kensbunker/micronaut/broker/QuotesController.java
+++ src/main/java/com/kensbunker/micronaut/broker/QuotesController.java
@@ -1,8 +1,10 @@
 package com.kensbunker.micronaut.broker;
 
+import com.kensbunker.micronaut.broker.error.CustomError;
 import com.kensbunker.micronaut.broker.model.Quote;
 import com.kensbunker.micronaut.broker.store.InMemoryStore;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import java.util.Optional;
@@ -19,6 +21,14 @@ public class QuotesController {
   @Get("/{symbol}")
   public HttpResponse getQuote(String symbol) {
     Optional<Quote> maybeQuote = store.fetchQuote(symbol);
+    if (!maybeQuote.isPresent()) {
+      final CustomError notFound = CustomError.builder().status(HttpStatus.NOT_FOUND.getCode())
+          .error(HttpStatus.NOT_FOUND.name())
+          .message("quote for symbol not available")
+          .path("/quotes/" + symbol)
+          .build();
+      return HttpResponse.notFound(notFound);
+    }
     return HttpResponse.ok(maybeQuote.get());
   }
 }
--- src/test/java/com/kensbunker/micronaut/broker/QuotesControllerTest.java
+++ src/test/java/com/kensbunker/micronaut/broker/QuotesControllerTest.java
@@ -1,16 +1,23 @@
 package com.kensbunker.micronaut.broker;
 
 import static io.micronaut.http.HttpRequest.GET;
+import static io.micronaut.http.HttpRequest.HEAD;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.kensbunker.micronaut.broker.error.CustomError;
 import com.kensbunker.micronaut.broker.model.Quote;
 import com.kensbunker.micronaut.broker.model.Symbol;
 import com.kensbunker.micronaut.broker.store.InMemoryStore;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.RxHttpClient;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import javax.inject.Inject;
 import org.junit.jupiter.api.Test;
@@ -45,6 +52,22 @@ class QuotesControllerTest {
     final Quote amazonResult = client.toBlocking().retrieve(GET("/quotes/AMZN"), Quote.class);
     LOG.debug("Result: {}", amazonResult);
     assertThat(amazon).isEqualToComparingFieldByField(amazonResult);
+  }
+
+  @Test
+  void returnsNotFoundOnUnsupportedSymbol() {
+    try {
+      client.toBlocking().retrieve(GET("/quotes/UNSUPPORTED"));
+    } catch (HttpClientResponseException e) {
+      assertEquals(HttpStatus.NOT_FOUND, e.getResponse().getStatus());
+      LOG.debug("Body: {}", e.getResponse().getBody());
+      final Optional<CustomError> customError = e.getResponse().getBody(CustomError.class);
+      assertTrue(customError.isPresent());
+      assertEquals(404, customError.get().getStatus());
+      assertEquals("NOT_FOUND", customError.get().getError());
+      assertEquals("quote for symbol not available", customError.get().getMessage());
+      assertEquals("/quotes/UNSUPPORTED", customError.get().getPath());
+    }
   }
 
   private Quote initRandom(String symbol) {
