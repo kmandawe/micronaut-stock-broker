39a3d07b572593533c35dde17dc600ba935e6625
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-11-25 16:07:04 +0800

    Micronaut Data JPA - Filter

--- src/main/java/com/kensbunker/micronaut/broker/QuotesController.java
+++ src/main/java/com/kensbunker/micronaut/broker/QuotesController.java
@@ -12,12 +12,14 @@ import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -87,6 +89,10 @@ public class QuotesController {
   @Get("/jpa/ordered/asc")
   public List<QuoteDTO> orderedAsc() {
     return quotes.listOrderByVolumeAsc();
+  }
+  @Get("/jpa/volume/{volume}")
+  public List<QuoteDTO> volumeFilter(@PathVariable BigDecimal volume) {
+    return quotes.findByVolumeGreaterThanOrderByVolumeAsc(volume);
   }
 
 
